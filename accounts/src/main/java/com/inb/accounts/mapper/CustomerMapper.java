6e9ec39407e167b215b6e10aa0a8c1b736326294
Vivek Gohil <ghl_vivek@hotmail.com>
2024-08-17 18:23:50 +0530

    Create Mapper Classes

---
+++ accounts/src/main/java/com/inb/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,23 @@
+package com.inb.accounts.mapper;
+
+
+import com.inb.accounts.dto.CustomerDto;
+import com.inb.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+
+}
