d56363ea5ed2d99179c76f4599c1701ec227f24c
Vivek Gohil <ghl_vivek@hotmail.com>
2024-08-19 21:25:32 +0530

    Saving Accounts and Customer Details

--- accounts/src/main/java/com/inb/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/inb/accounts/service/impl/AccountsServiceImpl.java
@@ -1,8 +1,12 @@
 package com.inb.accounts.service.impl;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Service;
 
+import com.inb.accounts.constants.AccountsConstants;
 import com.inb.accounts.dto.CustomerDto;
+import com.inb.accounts.entity.Accounts;
 import com.inb.accounts.entity.Customer;
 import com.inb.accounts.mapper.CustomerMapper;
 import com.inb.accounts.repository.AccountsRepository;
@@ -22,6 +26,18 @@ public class AccountsServiceImpl implements  IAccountsService {
     public void createAccount(CustomerDto customerDto) {
         Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
         Customer savedCustomer = customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(savedCustomer));
+    }
+
+    private Accounts createNewAccount(Customer customer){
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccountNumber = 1000000000L + new Random().nextInt(90000000);
+
+        newAccount.setAccountNumber(randomAccountNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        return newAccount;
     }
 
 }
