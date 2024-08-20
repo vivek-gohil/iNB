56e00fe4744536b8d92d4c7aa7e1875b0cebcef3
Vivek Gohil <ghl_vivek@hotmail.com>
2024-08-20 20:18:48 +0530

    Modifiy AccountsController - add createAccount mapping

--- accounts/src/main/java/com/inb/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/inb/accounts/controller/AccountsController.java
@@ -1,14 +1,31 @@
 package com.inb.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.inb.accounts.constants.AccountsConstants;
+import com.inb.accounts.dto.CustomerDto;
+import com.inb.accounts.dto.ResponseDto;
+import com.inb.accounts.service.IAccountsService;
+
+import lombok.AllArgsConstructor;
 
 
 @RestController
+@RequestMapping(path="/api", produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
 public class AccountsController {
-    @GetMapping("sayHello")
-    public String sayHello() {
-        return "Hello World";
-    }
+
+    private IAccountsService iAccountsService;
     
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
+        iAccountsService.createAccount(customerDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(AccountsConstants.MESSAGE_201, AccountsConstants.MESSAGE_201));
+    }
 }
