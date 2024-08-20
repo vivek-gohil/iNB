d6bcbe1bc2cbf7d44afa42caaa5718d1e9077b40
Vivek Gohil <ghl_vivek@hotmail.com>
2024-08-20 20:04:23 +0530

    Create GlobalExceptionHandler class

---
+++ accounts/src/main/java/com/inb/accounts/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.inb.accounts.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import com.inb.accounts.dto.ErrorResponseDto;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomerAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException exception, WebRequest webRequest) {
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(  
+                webRequest.getDescription(false),
+                HttpStatus.BAD_REQUEST,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
+    }
+}
