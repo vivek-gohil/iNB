0dc2617f4e8b77b64de066f4b1b0ea5bdf8849e7
Vivek Gohil <ghl_vivek@hotmail.com>
2024-08-20 19:50:37 +0530

    Add new method into CustomerRepository

--- accounts/src/main/java/com/inb/accounts/repository/CustomerRepository.java
+++ accounts/src/main/java/com/inb/accounts/repository/CustomerRepository.java
@@ -1,5 +1,7 @@
 package com.inb.accounts.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.inb.accounts.entity.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
+    Optional<Customer> findByMobileNumber(String mobileNumber);
 }
