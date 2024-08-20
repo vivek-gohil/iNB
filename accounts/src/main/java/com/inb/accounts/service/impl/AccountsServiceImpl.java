1de28450a4e061cc9147ed1e73036b2a9d10c698
Vivek Gohil <ghl_vivek@hotmail.com>
2024-08-20 19:53:38 +0530

    Add find customer by mobile number into service class

--- accounts/src/main/java/com/inb/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/inb/accounts/service/impl/AccountsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.inb.accounts.service.impl;
 
+import java.util.Optional;
 import java.util.Random;
 
 import org.springframework.stereotype.Service;
@@ -8,6 +9,7 @@ import com.inb.accounts.constants.AccountsConstants;
 import com.inb.accounts.dto.CustomerDto;
 import com.inb.accounts.entity.Accounts;
 import com.inb.accounts.entity.Customer;
+import com.inb.accounts.exception.CustomerAlreadyExistsException;
 import com.inb.accounts.mapper.CustomerMapper;
 import com.inb.accounts.repository.AccountsRepository;
 import com.inb.accounts.repository.CustomerRepository;
@@ -25,6 +27,10 @@ public class AccountsServiceImpl implements  IAccountsService {
     @Override
     public void createAccount(CustomerDto customerDto) {
         Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if(optionalCustomer.isPresent()){
+            throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber " + customerDto.getMobileNumber());
+        }
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
     }
